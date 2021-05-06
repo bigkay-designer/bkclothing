8f68651480a4e0f0c5127a16eddd7f70b1a43951
khalid ibrahim <ibrahimkhalid478@gmail.com>
2021-05-06 02:28:10 +0100

    finished editing the sort and filter page

--- src/components/pages/SortFilter.js
+++ src/components/pages/SortFilter.js
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, {useState, useRef} from 'react'
 import {Button} from '@material-ui/core'
-import { Add, Close, Sort } from '@material-ui/icons'
+import { Add, Close, Sort, Remove } from '@material-ui/icons'
 import '../css/SortFilter.css'
 function SortFilter() {
+    const [openFilter, setOpenFilter] = useState(false)
+    const [genderEle, setGenderEle] = useState(false)
+    const [productTypeEle, setProductTyprEle] = useState(false)
+    const [brandEle, setBrandEle] = useState(false)
+
+    const openFilterHandler = ()=>{
+        setTimeout(()=>{
+            setOpenFilter(true)
+        }, 200)
+    }
     return (
         <div className="sort__filter">
-            <div className="sort__filter--btn">
+            <div onClick={openFilterHandler} className="sort__filter--btn">
                 <Sort />
                 <Button>Filter & Sort</Button>
 
             </div>
-            <div className="container">
+            <div className={`container ${openFilter && "show__filter__container"}`}>
                 <div className="sub__title">
                     <h3>Filter & Sort</h3>
-                    <Close />
+                    <Close className="close__icons" onClick={()=> setOpenFilter(false)} />
                 </div>
                 <div className="wrapper">
                     <div className="sort">
@@ -32,32 +42,37 @@ function SortFilter() {
                     <div className="filter">
                         <h3>filter by</h3>
                         <div className="gender filter__options">
-                            <div className="title">
+                            <div className="title" onClick={()=> setGenderEle(!genderEle)} >
                                 <h3>gender</h3>
-                                <Add />
+                                <Add className={`${genderEle && "show__add__icon" }`} />
+                                <Remove className={`remove__icon ${genderEle && "show__remove__icon"}`} />
                             </div>
-                            <div className="body">
+                            <div className={`body ${genderEle && "show__body"}`}>
                                 <p>men <span>(2000)</span> </p>
                                 <p>women <span>(1879)</span> </p>
                             </div>
                         </div>
                         <div className="product__type filter__options">
-                            <div className="title">
+                            <div className="title" onClick={() => setProductTyprEle(!productTypeEle)} >
                                 <h3>product type</h3>
-                                <Add />
+                                <Add className={`${productTypeEle && "show__add__icon" }`} />
+                                <Remove className={`remove__icon ${productTypeEle && "show__remove__icon"}`} />
+
                             </div>
-                            <div className="body">
+                            <div className={`body ${productTypeEle && "show__body"}`}>
                                 <p>shoes <span>(2980)</span> </p>
                                 <p>clothing <span>(3400)</span> </p>
                                 <p>Accessories <span>(1745)</span> </p>
                             </div>
                         </div>
                         <div className="brand filter__options">
-                            <div className="title">
+                            <div className="title" onClick={()=> setBrandEle(!brandEle)} >
                                 <h3>brand</h3>
-                                <Add />
+                                <Add className={`${brandEle && "show__add__icon" }`} />
+                                <Remove className={`remove__icon ${brandEle && "show__remove__icon"}`} />
+
                             </div>
-                            <div className="body">
+                            <div className={`body ${brandEle && "show__body"}`}>
                                 <form>
                                     <div className="input">
                                         <input type="checkbox" name="adidas"/>
