1ed4b07b97f311560a52d62ee70a27c1d0b6c120
khalid ibrahim <ibrahimkhalid478@gmail.com>
2021-07-27 21:20:35 +0100

    styled login and signup pages

--- backend/models/products.js
+++ backend/models/products.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const products = new Schema({
     productName: String,
     productType: String,
+    productCategory: String,
     productBrand: String,
     productDesc: String,
     productGender: String,
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -23,18 +23,29 @@ router.route('/get/product/:name/:id').get( async (req, res) => {
 })
 
 // post product router
-router.route('/post').post((req, res)=> {
+router.route('/product/post').post((req, res)=> {
   const {
     productName, productType, 
     productBrand, productDesc, 
-    productGender, productPrice, productImage} = req.body;
+    productGender, productPrice, productImage, productCategory} = req.body;
       
-  const newProduct = new products({productName, productType, productBrand, productDesc, productGender, productPrice, productImage})
+  const newProduct = new products({productName, productType,productCategory, productBrand, productDesc, productGender, productPrice, productImage})
   newProduct.save()
   .then(resData => {
       res.status(200).json(resData)
   })
   .catch(err => res.status(500).json({error: err.message}))
+})
+
+
+///// specific product by title
+router.route('/get/:productName').get( async(req, res) => {
+  try{
+      const currentProducts = await products.find({productName: req.params.productName}).sort({_id: -1})
+      res.status(200).json(currentProducts)
+  }catch (err){
+      res.status(500).json({error: err.message})
+  }
 })
 
 export default router
