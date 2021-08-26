e10c191d095235fb4e933dc063e4e429757e7f7e
khalid ibrahim <ibrahimkhalid478@gmail.com>
2021-08-26 15:59:46 +0100

    changed banner image

--- client/src/components/landingPages/ShowCase.js
+++ client/src/components/landingPages/ShowCase.js
@@ -1,29 +1,32 @@
-import React from 'react'
-import banner from '../images/banner3.jpeg'
-import {Button} from '@material-ui/core'
-import '../css/Showcase.css'
+import React from 'react';
+import banner from '../images/bkclothing-banner.png';
+import { Button } from '@material-ui/core';
+import '../css/Showcase.css';
 function ShowCase() {
-    return (
-        <div className="showcase">
-            <div className="container">
-                <div className="banner">
-                    <img src={banner} alt="banner"/>
-                </div>
-                <div className="content__wrapper">
-                    <div className="content">
-                        <div className="title">
-                            <h2>Fashion sale</h2>
-                        </div>
-                        <div className="body">
-                            <h3>minimal menz style</h3>
-                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id, provident.</p>
-                            <Button>Show now</Button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="showcase">
+      <div className="container">
+        <div className="banner">
+          <img src={banner} alt="banner" />
         </div>
-    )
+        <div className="content__wrapper">
+          <div className="content">
+            <div className="title">
+              <h2>Fashion sale</h2>
+            </div>
+            <div className="body">
+              <h3>minimal menz style</h3>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id,
+                provident.
+              </p>
+              <Button>Show now</Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ShowCase
+export default ShowCase;
